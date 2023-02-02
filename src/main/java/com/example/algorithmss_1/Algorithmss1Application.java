b06f6fcc0d67e34c8cb8cdc83877dbfd4da3684b
Vereshchagin_Mikhail <farasers@mail.ru>
2023-02-02 05:36:17 +0300

    Алгоритмы 1

---
+++ src/main/java/com/example/algorithmss_1/Algorithmss1Application.java
@@ -0,0 +1,13 @@
+package com.example.algorithmss_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Algorithmss1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Algorithmss1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/algorithmss_1/Algorithmss1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.algorithmss_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Algorithmss1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
